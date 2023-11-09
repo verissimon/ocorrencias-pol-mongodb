9fab09d3716089f66cd6a4d50c0d2045269d9767
Veríssimo Terceiro <terceiroverissimo@gmail.com>
2023-11-09 03:26:20 -0300

    adicionando restricoes ao schema ocorrencias

--- src/model/Ocorrencia.ts
+++ src/model/Ocorrencia.ts
@@ -1,22 +1,40 @@
-import {model, Schema} from "mongoose"
-
-const Ocorrencia = model('Ocorrencia',
-    new Schema({
-        titulo: String,
-        tipo: String,
-        data: Date,
-        geom: {
-            type: {
-              type: String,
-              enum: ['Point'],
-              required: true
-            },
-            coordinates: {
-              type: [Number],
-              required: true
-            }
-        }
-    })
+import { model, Schema } from "mongoose"
+const ocorrenciaSchema = new Schema(
+  {
+    titulo: {
+      type: String,
+      required: true
+    },
+    tipo: {
+      type: String,
+      required: true
+    },
+    data: {
+      type: Date,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      immutable: true,
+      default: () => Date.now()
+    },
+    updatedAt: {
+      type: Date,
+      default: () => Date.now()
+    },
+    geom: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    }
+  }
 )
+const Ocorrencia = model('Ocorrencia', ocorrenciaSchema)
 
 export default Ocorrencia
