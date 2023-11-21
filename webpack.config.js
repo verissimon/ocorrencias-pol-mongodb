363f11ab40b86e675141bdc78fa64ce9d267db83
mikael <mikael.stl05@gmail.com>
2023-11-21 19:12:03 -0300

    adicionando elementos front-end à pasta public/

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,10 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src','public'),
   },
   devServer: {
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'src','public'),
     compress: true,
     port: 8080,
     open: true
