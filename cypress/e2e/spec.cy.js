583e8a318d858d01057f8858c3faea510fcdc005
Marianna <Marianna.lopes@academico.ifpb.edu.br>
2023-12-10 15:43:27 -0300

    Implementando testes para update e caso de erro em registro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,7 +19,38 @@ describe('Index Page', () => {
 
         cy.get('p').should('contain', 'Título: Example Title');
         cy.get('p').should('contain', 'Tipo: Assalto');
-        cy.get('p').should('contain', `Data: 1/1/2022, 12:00:00 PM`);
+        cy.get('p').should('contain', `Data: 01/01/2022, 12:00:00`);
+    });
+    
+    it('Deve ser capaz de atualizar um registro do mapa', () => {
+        cy.visit('http://localhost:8080/');
+        cy.wait(2000);
+
+        cy.get('#map').should('be.visible').then(() => {
+            cy.get('#map').click(testPoint.x, testPoint.y - 15);
+        });
+
+        cy.get('.update').should('be.visible').then(() => {
+            cy.get('#titulo').clear();
+            cy.get('#titulo').type('Example Occurrence');
+            cy.get('#tipo').select('Homicídio');
+            cy.get('#data').type('2022-01-01T12:00');
+
+            cy.get('.update').click();
+        });
+
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('Ocorrência atualizada com sucesso');
+        });
+
+        cy.reload();
+        cy.wait(2000);
+        cy.get('#map').click(testPoint.x, testPoint.y - 15);
+
+        cy.get('p').should('contain', 'Título: Example Occurrence');
+        cy.get('p').should('contain', 'Tipo: Homicídio');
+        cy.get('p').should('contain', `Data: 01/01/2022, 12:00:00`);
+ 
     });
 
     it('Deve ser capaz de deletar um registro no mapa', () => {
@@ -37,4 +68,22 @@ describe('Index Page', () => {
             expect(message).to.equal('Ocorrência deletada com sucesso');
         });
     });
+
+    it('Deve retornar um erro ao registrar uma ocorrência sem marcar o local no mapa', () => {
+        cy.visit('http://localhost:8080/');
+        cy.wait(2000);
+
+        cy.get('#map').should('be.visible').then(() => {
+            cy.get('#titulo').type('Example Title');
+            cy.get('#tipo').select('Assalto');
+            cy.get('#data').type('2022-01-01T12:00');
+
+            cy.get('#register').click();
+        });
+
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('Selecione um local no mapa antes de registrar');
+        });
+    });
+    
 });
--- cypress/e2e/spec.cy.test.js
+++ cypress/e2e/spec.cy.test.js
@@ -18,6 +18,24 @@ describe('Index Page', () => {
         cy.get('#register').click();
     });
 
+    it('Deve ser capaz de atualizar um registro no mapa', () =>{
+        cy.get('#map').should('be.visible').then(() => {
+            cy.get('#map').click(testPoint.x, testPoint.y);
+        });
+
+        cy.get('.update').should('be.visible').then(() => {
+            cy.get('#titulo').type('Example Occurrence');
+            cy.get('#tipo').select('Homicídio');
+            cy.get('#data').type('2022-01-01T12:00');
+
+            cy.get('.update').click();
+        });
+
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('Ocorrência atualizada com sucesso');
+        });
+    });
+
     it('Deve ser capaz de deletar um registro no mapa', () => {
         cy.get('#map').should('be.visible').then(() => {
             cy.get('#map').click(testPoint.x - 10, testPoint.y - 10);
@@ -29,6 +47,21 @@ describe('Index Page', () => {
 
         cy.on('window:alert', (message) => {
             expect(message).to.equal('Ocorrência deletada com sucesso');
+        });
+    });
+
+    it('Deve retornar um erro ao registrar uma ocorrência sem marcar o local no mapa', () => {
+
+        cy.get('#map').should('be.visible').then(() => {
+            cy.get('#titulo').type('Example Title');
+            cy.get('#tipo').select('Assalto');
+            cy.get('#data').type('2022-01-01T12:00');
+
+            cy.get('#register').click();
+        });
+
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('Selecione um local no mapa antes de registrar');
         });
     });
 });
